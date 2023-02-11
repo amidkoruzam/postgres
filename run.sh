ac66057c4dc7e44202e883df38a6a45fdd221f0e
Dmitrii Mazurok <tmazurok@gmail.com>
2023-02-11 16:32:11 +0300

    connect to music db after start up

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 container_id=$(docker run -d --rm -e POSTGRES_PASSWORD=password -v ${PWD}/db.sql:/docker-entrypoint-initdb.d/db.sql postgres)
-docker exec -it $container_id bash 
+docker exec -it $container_id bash -c "while !(pg_isready -d music); do sleep 1; done; psql -U postgres -d music"
 docker stop $container_id
