eb7c1c49110e57d5a4fc6fed058dbd9a7514cdb9
Dmitrii Mazurok <tmazurok@gmail.com>
2023-02-13 13:37:56 +0300

    replace self made db with more complex one

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 container_id=$(docker run -d --rm -e POSTGRES_PASSWORD=password -v ${PWD}/db.sql:/docker-entrypoint-initdb.d/db.sql postgres:15.2)
-docker exec -it $container_id bash -c "while !(pg_isready -d music); do sleep 1; done; psql -U postgres -d music"
+docker exec -it $container_id bash -c "while !(pg_isready -d postgres); do sleep 1; done; psql -U postgres -d postgres"
 docker stop $container_id
